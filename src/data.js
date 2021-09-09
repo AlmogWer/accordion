69a3b4eece9406f4d0c5c1d749b358b508d11418
Almog Wertzberger <Almogish@gmail.com>
2021-09-09 16:53:07 +0300

    initial

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Q qui aperiam neque provident nulla vel autem perspiciatis iusto saepe.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae veritatis qui aperiam neque  nulla vel autem perspiciatis iusto saepe.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae veritatis qui aperiam neque provident nulla vel autem  iusto saepe.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae  qui aperiam neque provident nulla vel autem perspiciatis iusto saepe.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae veritatis qui aperiam neque provident nulla vel  perspiciatis iusto saepe. ",
+  },
+];
+export default questions;
